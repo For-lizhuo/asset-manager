d944217df0acc1d69fa4c8ed7d7799f7086b6ed6
For-lizhuo <2541778295@qq.com>
2025-09-07 01:21:16 +0800

    feat: 增加项目根路径

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,40 +1,46 @@
-import { createBrowserRouter } from 'react-router-dom'
-import Layout from '../components/Layout'
-import Home from '../pages/Home'
-import AssetDetail from '../pages/AssetDetail'
-import AddAsset from '../pages/AddAsset'
-import EditAsset from '../pages/EditAsset'
-import AddHolding from '../pages/AddHolding'
+import { createBrowserRouter } from "react-router-dom";
+import Layout from "../components/Layout";
+import Home from "../pages/Home";
+import AssetDetail from "../pages/AssetDetail";
+import AddAsset from "../pages/AddAsset";
+import EditAsset from "../pages/EditAsset";
+import AddHolding from "../pages/AddHolding";
+import { rootPath } from "../../config";
 
-export const router = createBrowserRouter([
+export const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          index: true,
+          element: <Home />,
+        },
+        {
+          path: "add-asset",
+          element: <AddAsset />,
+        },
+        {
+          path: "assets/:id",
+          element: <AssetDetail />,
+        },
+        {
+          path: "edit-asset/:id",
+          element: <EditAsset />,
+        },
+        {
+          path: "add-holding",
+          element: <AddHolding />,
+        },
+        {
+          path: "edit-holding/:id",
+          element: <AddHolding />,
+        },
+      ],
+    },
+  ],
   {
-    path: '/',
-    element: <Layout />,
-    children: [
-      {
-        index: true,
-        element: <Home />,
-      },
-      {
-        path: 'add-asset',
-        element: <AddAsset />,
-      },
-      {
-        path: 'assets/:id',
-        element: <AssetDetail />,
-      },
-      {
-        path: 'edit-asset/:id',
-        element: <EditAsset />,
-      },
-      {
-        path: 'add-holding',
-        element: <AddHolding />,
-      },
-      {
-        path: 'edit-holding/:id',
-        element: <AddHolding />,
-      },
-    ],
-  },
-])
+    basename: `/${rootPath}`,
+  }
+);
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { rootPath } from './config'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: `/${rootPath}/`,
   plugins: [react()],
   server: {
     host: true,
